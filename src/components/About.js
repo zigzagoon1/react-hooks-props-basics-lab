37f315087158e3969e85b3b3fd73681be025a704
KellyMark <kmark602@gmail.com>
2023-02-11 14:58:16 -0500

    commit tree d801f69d188e66c9b508df13f4646d6ae65585ec

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,25 @@
 import React from "react";
-
-function About() {
+import Links from "./Links";
+function About(props) {
+  if (!props.bio || props.bio === "") {
+    return (
+      <div id="about">
+      <h2>About Me</h2>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {<Links github={props.github} linkedin={props.linkedin}/>}
+    </div>
+    );
+  }
+  else { 
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {<Links github={props.github} linkedin={props.linkedin}/>}
     </div>
   );
+  }
 }
 
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
